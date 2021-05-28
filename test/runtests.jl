efd4d6f326690c0f08953e5ce0e648e1b0a6e7f3
Neel Smith <dnsmith.neel@gmail.com>
2021-05-28 09:03:43 -0400

    typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
-using AtticGreek
+using HmtMSOrthography
 using Test
 using Orthography, PolytonicGreek
 using Unicode
 
 
-include("test_orthosystem.jl")
-include("test_orthography.jl")
-include("test_accents.jl")
-include("test_quantity.jl")
-include("test_syllabify.jl")
-include("test_augment.jl")
+#include("test_orthosystem.jl")
+##include("test_orthography.jl")
+include("test_accentuation.jl")
+#include("test_quantity.jl")
+#include("test_syllabify.jl")
+#include("test_augment.jl")
